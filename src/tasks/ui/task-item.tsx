d2e58009f5d23d100e1b9f37033299df1d686440
Valeriy Yeroshenko <valeriy.yeroshenko@gmail.com>
2023-10-30 14:16:59 +0200

    ref: di-fix

--- src/tasks/ui/task-item.tsx
+++ src/tasks/ui/task-item.tsx
@@ -1,28 +1,20 @@
-import { UserSelect } from "../../user/ui/user-select";
+import {ReactNode} from "react";
 
-export const TaskItem = ({
-  title,
-  done,
-  onDelete,
-  onToggleDone,
-  onChangeOwner,
-  ownerId,
-}: {
+export const TaskItem = ({title, done, onDelete, onToggleDone, userSelectSlot}: {
   title: string;
   done: boolean;
-  ownerId?: string;
-  onChangeOwner: (ownerId: string) => void;
+  userSelectSlot: ReactNode,
   onToggleDone: () => void;
   onDelete: () => void;
 }) => {
   return (
-    <div style={{ display: "flex", gap: "10px", padding: "10px" }}>
+    <div style={{display: "flex", gap: "10px", padding: "10px"}}>
       <label>
-        <input type="checkbox" checked={done} onChange={onToggleDone} />
+        <input type="checkbox" checked={done} onChange={onToggleDone}/>
         done
       </label>
-      <button onClick={() => onDelete()}>Delete task</button>
-      <UserSelect userId={ownerId} onChangeUserId={onChangeOwner} />
+      <button onClick={onDelete}>Delete task</button>
+      {userSelectSlot}
       <div>{title}</div>
     </div>
   );
